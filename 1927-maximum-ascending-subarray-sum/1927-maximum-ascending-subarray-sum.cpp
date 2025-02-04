16d506536632d73a39a0a2d08ede77139a39c610
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-02-04 20:35:48 +0530

    Time: 0 ms (100%), Space: 11.2 MB (12.23%) - LeetHub

--- 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.cpp
+++ 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.cpp
@@ -7,12 +7,9 @@ public:
 
         int maxSum = nums[0];
         int runSum = nums[0];
-        
+
         for(int i=1; i<n; i++){
-            runSum += nums[i];
-            if(nums[i] < nums[i-1] || nums[i] == nums[i-1]){
-                runSum = nums[i];
-            }
+            runSum = (nums[i] > nums[i-1]) ? runSum + nums[i] : nums[i];
             maxSum = max(maxSum, runSum);
         }
 
