02e2d4b2ec15806707ba5c6d9d1dc3fa6ff5c9d1
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-02-11 00:18:15 +0530

    Time: 0 ms (100%), Space: 8.3 MB (96.37%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.cpp
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
     string clearDigits(string s) {
-
-        stack<char> stk;
-        int n = s.size();
-
-        for(int i=0; i<n; i++){ // O(N)
-            if(isdigit(s[i])){
-                stk.pop();
-            }
-            else{
-                stk.push(s[i]);
-            }
-        }
-
+        
         string ans = "";
 
-        while(!stk.empty()){ //O(N)
-
-            char c = stk.top();
-
-            ans += c;
-            stk.pop();
-
+        for(int i=0; i<s.size(); i++){
+            if(isdigit(s[i])){
+                ans.erase(ans.begin() + ans.size()-1);
+            }else{
+                ans += s[i];
+            }
         }
-        reverse(ans.begin(), ans.end()); // O(log N)
 
         return ans;
-    
-        
     }
 };
