6e1b0961e19e69a619bca65c761cd19164901985
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-02-21 00:57:06 +0530

    Time: 0 ms (100%), Space: 12.5 MB (84%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-
-    // Simplify Brute Force
-
     string findDifferentBinaryString(vector<string>& nums) {
-                int n = nums.size();
-        unordered_set<int> seen;
 
-        // Convert all binary strings to decimal and store in set
-        for (const auto& str : nums) {
-            seen.insert(bitset<16>(str).to_ulong());
+        int n = nums.size();
+        string result = "";
+
+        for(int i=0; i<n; i++){
+
+            result += nums[i][i] == '1' ? '0' : '1';
+
         }
 
-        // Find the first missing number
-        for (int i = 0; i < (1 << n); i++) {
-            if (!seen.count(i)) {
-                return bitset<16>(i).to_string().substr(16 - n); // Return n-bit binary string
-            }
-        }
-        return ""; // Should never reach here
+        return result;
+        
     }
 };
