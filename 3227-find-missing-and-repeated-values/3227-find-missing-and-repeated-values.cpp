666c0cf8ab3a1ae65931d73419c9d8e42288bd65
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-03-06 22:08:37 +0530

    Time: 6 ms (71.7%), Space: 26.9 MB (72.97%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
@@ -1,48 +1,33 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        vector<int> temp;
 
         int n = grid.size();
-        set<int> ss;
 
+        vector<int> counts(n * n + 1, 0);
+        long long actual_sum = 0;
+
+        int repeated = 0;
 
         for(int i=0; i<n; i++){
-
             for(int j=0; j<n; j++){
-
-                temp.push_back(grid[i][j]);
-                ss.insert(grid[i][j]);
-
-
+               counts[ grid[i][j] ]++;
+                actual_sum += grid[i][j];
             }
         }
 
-        sort(temp.begin(), temp.end());
-
-        vector<int> ans(2);
-        ans[0] = INT_MAX;
-
-
-        for(int i=0; i<temp.size()-1; i++){
-
-            if(temp[i] == temp[i+1]){
-                ans[0] = min(ans[0], temp[i]);
+        for(int i=0; i<=n*n; i++){
+            if(counts[i] == 2) {
+                repeated = i;
+                break;
             }
-
-            if(ss.find(i+1) == ss.end()){
-                ans[1] = i+1;
-            }
-
         }
 
-        if(ss.find(temp.size()) == ss.end()){
-            ans[1] = temp.size();
-        }
+        long long expected_sum =  (long long )(n * n) * (n * n+1) /2;
+        int missing = expected_sum - actual_sum + repeated;
 
-
+        return {repeated, missing};
 
         
-        return ans;
     }
 };
