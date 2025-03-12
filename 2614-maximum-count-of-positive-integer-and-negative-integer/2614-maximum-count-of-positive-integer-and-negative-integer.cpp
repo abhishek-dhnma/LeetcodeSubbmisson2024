5ac2c63ecb74e42d38ef108353b1088b5535ebd1
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-03-13 00:24:37 +0530

    Time: 0 ms (100%), Space: 21.4 MB (75.85%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,26 +1,40 @@
 class Solution {
 public:
-    int maximumCount(vector<int>& nums) {
-        int n = nums.size();
-        int pc = 0;
-        int nc = 0;
 
-        int i = 0;
+    int BinarySearch(vector<int>& nums, int n, int target){
+        int left = 0;
+        int right = n-1;
 
-        while( i < n){
+        int result = n;
 
-            if(nums[i] < 0){
-                nc++;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(nums[mid] >= target){
+                result = mid;
+                right = mid - 1; // left
+            }else{
+                left = mid +1; // right
             }
-
-            if(nums[i] > 0){
-                pc++;
-            }
-
-            i++;
         }
 
-        return max(nc, pc);
+        return result;
+    }
+
+    int maximumCount(vector<int>& nums) {
+
+        int totalElements = nums.size();
+        
+        int firstzeroidx = BinarySearch(nums, totalElements, 0);
+        int firstpositiveidx = BinarySearch(nums, totalElements, 1);
+
+        int zerocount = firstpositiveidx - firstzeroidx;
+
+        int positivecount = totalElements - firstpositiveidx;
+        
+        int negativecount = totalElements - positivecount - zerocount;
+
+        return max(positivecount, negativecount);
+
         
     }
 };
