ca041b184db9258e5213bc497ed8f119d626dc04
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-03-29 01:57:40 +0530

    Time: 319 ms (19.93%), Space: 77.8 MB (19.93%) - LeetHub

--- 2588-maximum-number-of-points-from-grid-queries/2588-maximum-number-of-points-from-grid-queries.cpp
+++ 2588-maximum-number-of-points-from-grid-queries/2588-maximum-number-of-points-from-grid-queries.cpp
@@ -1,59 +1,52 @@
 class Solution {
 public:
-vector<vector<int>> direction = {{0,1}, {1,0}, {-1,0}, {0, -1}};
+    vector<vector<int>> direction = {{0,1}, {1,0}, {-1,0}, {0, -1}};
+
     vector<int> maxPoints(vector<vector<int>>& grid, vector<int>& queries) {
         int m = grid.size();
         int n = grid[0].size();
-
         int Q = queries.size();
-        vector<int> result(Q,0);
 
+        vector<int> result(Q, 0);
         vector<pair<int,int>> sortedQ;
-        for(int i=0; i <Q; i++){
+        
+        for(int i = 0; i < Q; i++) {
             sortedQ.push_back({queries[i], i});
         }
 
-        sort(begin(sortedQ), end(sortedQ));
+        sort(sortedQ.begin(), sortedQ.end());
 
         priority_queue<vector<int>, vector<vector<int>>, greater<>> qp;
-
         vector<vector<bool>> visited(m, vector<bool>(n, false));
 
         int points = 0;
-
         qp.push({grid[0][0], 0, 0});
         visited[0][0] = true;
 
-        for(int i=0; i<Q; i++){
+        for(int i = 0; i < Q; i++) {
             int queryValue = sortedQ[i].first;
             int idx = sortedQ[i].second;
-            while(!qp.empty() && qp.top()[0] < queryValue){
-                int i = qp.top()[1];
-                int j = qp.top()[2];
-
+            
+            while (!qp.empty() && qp.top()[0] < queryValue) {
+                auto top = qp.top();
                 qp.pop();
 
+                int x = top[1], y = top[2];
                 points++;
 
-                for(auto& dir : direction){
-                    int i_ = i + dir[0];
-                    int j_ = j + dir[1];
+                for (auto& dir : direction) {
+                    int nx = x + dir[0], ny = y + dir[1];
 
-                    if (i_ >= 0 && i_ < m && j_ >= 0 && j_ <n && !visited[i_][j_]){
-                        qp.push({grid[i_][j_], i_, j_});
-                        visited[i_][j_] = true;
+                    if (nx >= 0 && nx < m && ny >= 0 && ny < n && !visited[nx][ny]) {
+                        visited[nx][ny] = true; // **Fix: Assign true correctly**
+                        qp.push({grid[nx][ny], nx, ny});
                     }
                 }
-
-                 
-
             }
 
             result[idx] = points;
         }
 
         return result;
-
-
     }
 };
