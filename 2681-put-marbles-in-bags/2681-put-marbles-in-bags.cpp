023a08e33a747fc0bfeb82a34c7a9f1763d5f627
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-04-01 02:08:59 +0530

    Time: 33 ms (76.7%), Space: 64.2 MB (91.1%) - LeetHub

---
+++ 2681-put-marbles-in-bags/2681-put-marbles-in-bags.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long putMarbles(vector<int>& weights, int k) {
+
+        int n = weights.size();
+        int m = n-1;
+
+     vector<int> pairSum(m, 0);
+
+     for(int i=0; i<m; i++){
+        pairSum[i] = weights[i] + weights[i+1];
+     }
+
+     sort(begin(pairSum), end(pairSum));
+
+     long maxSum = 0;
+     long minSum = 0;
+
+     for(int i=0; i<k-1; i++){
+        minSum += pairSum[i];
+        maxSum += pairSum[m-i-1];
+     }
+
+     return maxSum - minSum;
+        
+    }
+};
