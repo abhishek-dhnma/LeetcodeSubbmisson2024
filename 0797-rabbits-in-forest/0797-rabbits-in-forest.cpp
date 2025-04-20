cccf98d826eacafa57eeb02020cba86f34140390
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-04-20 23:38:47 +0530

    Time: 3 ms (22.67%), Space: 12.1 MB (91.6%) - LeetHub

---
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numRabbits(vector<int>& answers) {
+
+        int n = answers.size();
+        unordered_map<int, int> mp;
+
+        for(auto &x : answers){
+            mp[x]++;
+        }
+
+        int total = 0;
+
+        for(auto& it : mp){
+            int x = it.first;
+            int count = it.second;
+
+            int groupSize = x+1;
+            int groups = ceil( (double)count/groupSize);
+
+            total += groups * (groupSize);
+        }
+
+        return total;
+        
+    }
+};
