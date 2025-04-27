83b32fe853c1571b5461dddab9a300d7bf303bd4
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-04-27 20:38:58 +0530

    Time: 0 ms (100%), Space: 48.4 MB (45.56%) - LeetHub

--- 3685-count-subarrays-of-length-three-with-a-condition/3685-count-subarrays-of-length-three-with-a-condition.cpp
+++ 3685-count-subarrays-of-length-three-with-a-condition/3685-count-subarrays-of-length-three-with-a-condition.cpp
@@ -4,20 +4,16 @@ public:
         int n = nums.size();
         int count = 0;
 
-        for(int i=0; i<n; i++){
-            int j = i+1;
-            int k = i+2;
-                    if(j <n && k< n)
-                   { double sum = (double)nums[i] + nums[k];
-                    double half = (double)nums[j]/2;
-
-                    cout << sum << " " << half << "-" << i << j << k << endl;
-                    if( sum == half){
-                        count++;
-                    }}
+        for (int i = 0; i < n; i++) {
+            int j = i + 1;
+            int k = i + 2;
+            if (j < n && k < n) {
+                if ((double)nums[i] + nums[k] == (double)nums[j] / 2) {
+                    count++;
+                }
+            }
         }
 
         return count;
-        
     }
 };
