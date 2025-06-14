558968c91e612acfe525214c956328dbdddd1275
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-06-14 22:41:44 +0530

    Time: 0 ms (100%), Space: 27.4 MB (8.63%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,47 +1,41 @@
 class Solution {
 public:
+
+    int solve(vector<int>& height, int n){
+
+
+        vector<int> lmax(n,0);
+        vector<int> rmax(n,0);
+
+        int lmos = height[0];
+        for(int i=0; i<n; i++){
+            lmax[i] = max(lmos, height[i]);
+            lmos = lmax[i];
+        }
+
+        int rmos = height[n-1];
+        for(int i=n-1; i>=0; i--){
+            rmax[i] = max(rmos, height[i]);
+            rmos = rmax[i];
+        }
+
+
     
-    int WaterTrapped(vector<int> building_Heights, int n){
+        int ans = 0;
 
-    vector<int> lmax(n,0);
-    vector<int> rmax(n,0);
+        for(int i=0; i<n; i++){
+            ans += min(lmax[i], rmax[i]) - height[i];
+        }
 
-    int leftmaxsofar = building_Heights[0];
-    for(int i=0; i<n; i++){
-        lmax[i] = max(leftmaxsofar, building_Heights[i]);
-        leftmaxsofar = lmax[i];
+
+        return ans;
+
+
     }
 
-
-
-
-        int rightmaxsofar = building_Heights[n-1];
-    for(int i=n-1; i>=0; i--){
-        rmax[i] = max(rightmaxsofar, building_Heights[i]);
-        rightmaxsofar = rmax[i];
-    }
-
-
-    vector<int> ans(n, 0);
-
-    for(int i=1; i<n-1; i++){
-
-        ans[i] = min(lmax[i], rmax[i]) - building_Heights[i];
-    }
-
-
-    int TotalWaterTrappedBetweenBuildings = 0;
-
-    for(auto xx : ans){
-        TotalWaterTrappedBetweenBuildings += xx;
-    }
-
-    return TotalWaterTrappedBetweenBuildings;
-
-}
     int trap(vector<int>& height) {
+
+        return solve(height, height.size());
         
-         
-        return WaterTrapped(height, height.size());
     }
 };
