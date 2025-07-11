ea13a195aa63324da1569f964a7182ef9d39ad2b
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-07-12 03:04:01 +0530

    Time: 63 ms (94.45%), Space: 98.2 MB (75.17%) - LeetHub

--- 2479-meeting-rooms-iii/2479-meeting-rooms-iii.cpp
+++ 2479-meeting-rooms-iii/2479-meeting-rooms-iii.cpp
@@ -1,61 +1,67 @@
 class Solution {
 public:
-    int mostBooked(int totalRooms, vector<vector<int>>& meetings) {
 
-        // Step 1: Sort all meetings by their start time
+    typedef pair<long long, int> P;
+
+    int mostBooked(int n, vector<vector<int>>& meetings) {
+
+        int m = meetings.size();
+
         sort(meetings.begin(), meetings.end());
 
-        // roomEndTime[i] stores the time when room i becomes available
-        vector<long long> roomEndTime(totalRooms, 0); 
+        vector<int> roomsUsedCount(n,0);
 
-        // roomUsageCount[i] stores the number of times room i has been used
-        vector<long> roomUsageCount(totalRooms, 0);
+        priority_queue<P, vector<P>, greater<P>> usedRooms;
+        priority_queue<int, vector<int>, greater<int>> availableRooms;
 
-        // Step 2: Process each meeting one by one
-        for (int i = 0; i < meetings.size(); i++) {
-            int currentStart = meetings[i][0];         // Meeting start time
-            int currentEnd = meetings[i][1];           // Meeting end time
-            int meetingDuration = currentEnd - currentStart;
 
-            bool roomAssigned = false;                 // Flag to check if we found a free room
-            long long earliestAvailableTime = LLONG_MAX;       // Track the earliest time any room becomes free
-            int earliestRoomIndex = 0;                 // Track the room that becomes free earliest
+        for(int room =0; room <n; room++){
+            availableRooms.push(room);
+        }
 
-            // Step 3: Try to assign the current meeting to an available room
-            for (int roomIndex = 0; roomIndex < totalRooms; roomIndex++) {
-                // If the room is free before or exactly at the meeting start time
-                if (roomEndTime[roomIndex] <= currentStart) {
-                    roomEndTime[roomIndex] = currentEnd;    // Book the room till current meeting's end
-                    roomUsageCount[roomIndex]++;            // Increase usage count
-                    roomAssigned = true;                    // Mark as assigned
-                    break;                                  // No need to check other rooms
-                }
 
-                // Track the room that will be free the earliest (for later use)
-                if (roomEndTime[roomIndex] < earliestAvailableTime) {
-                    earliestAvailableTime = roomEndTime[roomIndex];
-                    earliestRoomIndex = roomIndex;
-                }
+        for(vector<int> & meet : meetings){
+            int start = meet[0];
+            int end = meet[1];
+            int duration = end - start;
+
+            while(!usedRooms.empty() && usedRooms.top().first <= start){
+                int room = usedRooms.top().second;
+                usedRooms.pop();
+                availableRooms.push(room);
             }
 
-            // Step 4: If no room was available, delay the meeting and assign it to the earliest free room
-            if (!roomAssigned) {
-                roomEndTime[earliestRoomIndex] += meetingDuration; // Start after current booking ends
-                roomUsageCount[earliestRoomIndex]++;               // Count the usage
+            if(!availableRooms.empty()){
+                int room = availableRooms.top();
+                availableRooms.pop();
+
+                usedRooms.push({end, room});
+                roomsUsedCount[room]++;
+            }else{
+
+                int room = usedRooms.top().second;
+                long long endTime = usedRooms.top().first;
+
+                usedRooms.pop();
+                usedRooms.push({endTime + duration, room});
+                roomsUsedCount[room]++;
+
+            }
+
+
+        }
+
+        int result = -1;
+        int maxUse = 0;
+
+        for(int room = 0; room <n; room++){
+            if(roomsUsedCount[room] > maxUse){
+                maxUse = roomsUsedCount[room];
+                result = room;
             }
         }
 
-        // Step 5: Find the room that was used the most
-        int mostUsedRoom = -1;
-        int maxUsage = 0;
-
-        for (int roomIndex = 0; roomIndex < totalRooms; roomIndex++) {
-            if (roomUsageCount[roomIndex] > maxUsage) {
-                maxUsage = roomUsageCount[roomIndex];
-                mostUsedRoom = roomIndex;
-            }
-        }
-
-        return mostUsedRoom;
+    return result;
+        
     }
 };
