8b8857fcb706de129a0976e8979131b7452c169b
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-07-15 09:28:29 +0530

    Time: 0 ms (100%), Space: 8 MB (67.48%) - LeetHub

--- 3396-valid-word/3396-valid-word.cpp
+++ 3396-valid-word/3396-valid-word.cpp
@@ -2,24 +2,35 @@ class Solution {
 public:
     bool isValid(string word) {
 
-         if (word.size() < 3)
+        if(word.size() < 3){
             return false;
+        }
 
-        // Disallow special characters
-        regex specialCharsPattern("[@#$]");
-        if (regex_search(word, specialCharsPattern))
-            return false;
+        bool hasVowel = 0;
+        bool hasConsonant = 0;
 
-        // At least one vowel (case-insensitive)
-        regex vowelPattern("[aeiouAEIOU]");
-        if (!regex_search(word, vowelPattern))
-            return false;
+        for(char &  ch : word){
 
-        // At least one consonant (case-insensitive)
-        regex consonantPattern("[b-df-hj-np-tv-zB-DF-HJ-NP-TV-Z]");
-        if (!regex_search(word, consonantPattern))
-            return false;
+            if(isalpha(ch)){
+                ch = tolower(ch);
 
-        return true;
+                if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                    hasVowel = true;
+                    
+                }else{
+                    hasConsonant = true;
+                }
+            }else if(!isdigit(ch)){
+                return false;
+            }
+
+        }
+
+
+        return  hasVowel && hasConsonant;
+
+        
+
+        
     }
 };
