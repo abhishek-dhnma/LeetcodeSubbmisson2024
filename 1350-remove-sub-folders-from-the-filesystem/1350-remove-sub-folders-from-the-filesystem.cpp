6f5cb38839663271c246206d0d45d39313e00c73
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-07-20 02:01:05 +0530

    Time: 100 ms (42.46%), Space: 77.5 MB (30.79%) - LeetHub

--- 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
@@ -4,41 +4,32 @@ public:
 
         unordered_set<string> fldr(folder.begin(), folder.end());
 
-
         vector<string> ans;
 
-
-        for(int i=0; i<folder.size(); i++){
+        for (int i = 0; i < folder.size(); i++) {
 
             string currfolder = folder[i];
 
-            bool isSub= false;
+            bool isSub = false;
 
-            
-
-            while(!currfolder.empty()){
+            while (true) {
 
                 size_t position = currfolder.find_last_of('/');
+                if (position == 0) break;  // reached root level like "/a"
 
                 currfolder = currfolder.substr(0, position);
 
-                if(fldr.find(currfolder)!= fldr.end()){
+                if (fldr.find(currfolder) != fldr.end()) {
                     isSub = true;
                     break;
                 }
-                 
-
-               
             }
 
-            if(!isSub){
+            if (!isSub) {
                 ans.push_back(folder[i]);
             }
-
-            
         }
 
         return ans;
-        
     }
 };
