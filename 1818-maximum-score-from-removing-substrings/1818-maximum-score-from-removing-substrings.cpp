5a1f014df66d1248e85fe422c0bc4801632aade9
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-07-23 15:35:54 +0530

    Time: 624 ms (5.14%), Space: 19.9 MB (94.86%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
@@ -1,69 +1,50 @@
 class Solution {
 public:
+    int result = 0;
+
+    void solve(string& s, int n, int val, char ch1, char ch2) {
+
+        for (int i = 0; i < n - 1; i++) {
+
+            while (s[i] == ch1 && s[i + 1] == ch2) {
+                s.erase(i, 2);
+                result += val;
+                i = max(0, i - 2);
+                n -= 2;
+            }
+        }
+    }
+
     int maximumGain(string s, int x, int y) {
 
-        int result= 0;
-
-        char ch1,ch3;
+        char ch1, ch3;
         char ch2, ch4;
 
-        int mx_val = max(x,y);
-        int mn_val = min(x,y);
+        int mx_val = max(x, y);
+        int mn_val = min(x, y);
 
-        
-        if(x > y){
+        if (x > y) {
             ch1 = 'a';
             ch2 = 'b';
-        }else{
+        } else {
             ch1 = 'b';
             ch2 = 'a';
         }
 
-
-        
         int n = s.size();
 
-        //max nikalunga
-        int i = 0; 
-        for(; i < n-1; i++){
+        solve(s, n, mx_val, ch1, ch2);
 
-            while(s[i] == ch1 && s[i+1] == ch2){
-                s.erase(i , 2);
-                result += mx_val;
-                i = max(0, i-2);
-                n -= 2;
-            }
-
- 
+        if (ch1 == 'a' && ch2 == 'b') {
+            ch1 = 'b';
+            ch2 = 'a';
+        } else {
+            ch1 = 'a';
+            ch2 = 'b';
         }
 
-        // min niklaunga
-
-       if(ch1 == 'a' && ch2 == 'b'){
-         ch1 = 'b';
-         ch2 = 'a';
-       }else{
-        ch1 = 'a';
-        ch2 = 'b';
-       }
-
-
-        for(int i=0; i < n-1 ; i++){
-
-            while(s[i] == ch1 && s[i+1] == ch2){
-                s.erase(i , 2);
-                result += mn_val;
-                i = max(0, i-2);
-                n -= 2;
-            }
-
-        }
-
-
-
-        //result
+        solve(s, n, mn_val, ch1, ch2);
 
         return result;
-        
     }
 };
