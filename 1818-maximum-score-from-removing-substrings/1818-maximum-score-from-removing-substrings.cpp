875e55ff658e87510c4902370b3d4cd35fb44de3
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-07-23 16:22:13 +0530

    Time: 623 ms (5.14%), Space: 19.7 MB (97%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int result = 0;
 
-    void solve(string& s, int n, int val, char ch1, char ch2) {
+    void removePairs(string& s, int & n, int val, char ch1, char ch2) {
 
         for (int i = 0; i < n - 1; i++) {
 
             while (s[i] == ch1 && s[i + 1] == ch2) {
                 s.erase(i, 2);
                 result += val;
-                i = max(0, i - 2);
+                i = max(0, i - 2);// Rewind to check overlapping pairs
                 n -= 2;
             }
         }
@@ -17,33 +17,25 @@ public:
 
     int maximumGain(string s, int x, int y) {
 
-        char ch1, ch3;
-        char ch2, ch4;
-
-        int mx_val = max(x, y);
-        int mn_val = min(x, y);
-
+        char ch1;
+        char ch2;
+        int n = s.size();
+// Determine which pair to prioritize based on the higher value
         if (x > y) {
             ch1 = 'a';
             ch2 = 'b';
         } else {
             ch1 = 'b';
             ch2 = 'a';
+            swap(x,y); // Swap values so x is always the higher one
         }
 
-        int n = s.size();
+        
 
-        solve(s, n, mx_val, ch1, ch2);
+        removePairs(s, n, x, ch1, ch2);
 
-        if (ch1 == 'a' && ch2 == 'b') {
-            ch1 = 'b';
-            ch2 = 'a';
-        } else {
-            ch1 = 'a';
-            ch2 = 'b';
-        }
+        removePairs(s, n, y, ch2, ch1);// Reverse pair for second pass
 
-        solve(s, n, mn_val, ch1, ch2);
 
         return result;
     }
