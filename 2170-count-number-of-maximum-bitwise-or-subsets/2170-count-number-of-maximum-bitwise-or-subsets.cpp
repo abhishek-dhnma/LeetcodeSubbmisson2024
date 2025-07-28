5bf42ef1a45c35f98ca448b833d504aa3715977f
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-07-29 04:03:17 +0530

    Time: 19 ms (48.05%), Space: 10.7 MB (65.64%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -5,17 +5,15 @@ public:
     void solve(vector<int>& nums, int i, vector<int>& out, int maxOr) {
 
         if (i == nums.size()) {
+            // Handle empty subset case
             int loclOr = 0;
-
-            if(!out.empty()){
+            if (!out.empty()) {
                 loclOr = out[0];
 
-            for (int i = 1; i < out.size(); i++) {
-                loclOr  |= out[i];
+                for (int i = 1; i < out.size(); i++) {
+                    loclOr |= out[i];
+                }
             }
-
-            }
-            
 
             if (maxOr == loclOr) {
                 ansCount++;
@@ -24,38 +22,37 @@ public:
             return;
         }
 
+        // Include nums[i]
         out.push_back(nums[i]);
         solve(nums, i + 1, out, maxOr);
 
+        // Exclude nums[i]
         out.pop_back();
         solve(nums, i + 1, out, maxOr);
 
         return;
     }
     int countMaxOrSubsets(vector<int>& nums) {
-
-
+        // Handle empty input
         if (nums.empty()) {
             return 0;
         }
 
-        
-
+        // Compute maxOr
         int maxOr = nums[0];
         bool allSame = true;
-
         int n = nums.size();
 
         for (int i = 1; i < n; i++) {
             maxOr |= nums[i];
-            if (nums[i] != maxOr) {
+            if (nums[i] != nums[0]) {
                 allSame = false;
             }
         }
-        
 
+        // If all elements are identical, all non-empty subsets have the same OR
         if (allSame) {
-            return (1LL << nums.size()) - 1;
+            return (1LL << n) - 1;
         }
 
         vector<int> out;
