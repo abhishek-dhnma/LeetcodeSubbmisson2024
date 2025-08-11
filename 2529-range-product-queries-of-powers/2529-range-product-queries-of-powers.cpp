c67e28ac20eb6b6f72dfd04f4818ef5aeda725eb
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-08-11 23:05:02 +0530

    Time: 15 ms (75.53%), Space: 147.6 MB (58.51%) - LeetHub

---
+++ 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> productQueries(int n, vector<vector<int>>& queries) {
+
+        vector<int> powers;
+        vector<int> answer;
+
+        long long mod = 1e9 + 7;
+
+        for (int i = 0; i < 32; i++) {
+
+            if (n & 1) {
+
+                powers.push_back(pow(2, i));
+            }
+            n = (n >> 1);
+        }
+
+        for(auto & p : powers){
+            cout << p << " ";
+        }
+
+        for (auto& q : queries) {
+
+            int l = q[0];
+            int r = q[1];
+            long long pdt = powers[l];
+
+            for (int j = l + 1; j <= r; j++) {
+
+                pdt = (pdt* powers[j]) % mod;
+            }
+
+            answer.push_back(pdt);
+        }
+
+        return answer;
+    }
+};
