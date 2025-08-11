a9a5011ff1af23c61d82bd7d76743d3f086b9554
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-08-12 00:21:18 +0530

    Time: 5 ms (92.55%), Space: 147.8 MB (45.21%) - LeetHub

--- 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.cpp
+++ 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.cpp
@@ -1,11 +1,56 @@
 class Solution {
 public:
+    long long mod = 1e9 + 7;
+
+    void buildSegmentTree(vector<long long>& segmentTree, vector<long long>& powers, int i,
+                          int l, int r) {
+
+        if (l == r) {
+            segmentTree[i] = powers[l];
+            return;
+        }
+
+        int mid = l + (r - l) / 2;
+        // left
+        buildSegmentTree(segmentTree, powers, 2 * i + 1, l, mid);
+
+        // right
+        buildSegmentTree(segmentTree, powers, 2 * i + 2, mid + 1, r);
+
+        segmentTree[i] =
+            (segmentTree[2 * i + 1] * segmentTree[2 * i + 2]) % mod;
+    }
+
+    long long productQueriesSegment(int start, int end,
+                                    vector<long long>& segmentTree, int i, int l,
+                                    int r) {
+
+        // out of bound
+
+        if (r < start || l > end) {
+            return 1;
+        }
+
+        // complete inside
+
+        else if (start <= l && r <= end) {
+            return segmentTree[i];
+        }
+
+        // overlapping
+
+        int mid = l + (r - l) / 2;
+        // left
+        return (productQueriesSegment(start, end, segmentTree, 2 * i + 1, l, mid) *
+                productQueriesSegment(start, end, segmentTree, 2 * i + 2, mid + 1,
+                               r)) %
+               mod;
+    }
+
     vector<int> productQueries(int n, vector<vector<int>>& queries) {
 
-        vector<int> powers;
+        vector<long long> powers;
         vector<int> answer;
-
-        long long mod = 1e9 + 7;
 
         for (int i = 0; i < 32; i++) {
 
@@ -16,16 +61,18 @@ public:
             n = (n >> 1);
         }
 
+        int s = powers.size();
+        vector<long long> segmentTree(4 * s);
+
+        buildSegmentTree(segmentTree, powers, 0, 0, s - 1);
+
         for (auto& q : queries) {
 
             int l = q[0];
             int r = q[1];
-            long long pdt = powers[l];
 
-            for (int j = l + 1; j <= r; j++) {
-
-                pdt = (pdt* powers[j]) % mod;
-            }
+            long long pdt =
+                productQueriesSegment(l, r, segmentTree, 0, 0, s - 1);
 
             answer.push_back(pdt);
         }
