5a40d4f8c9b6552bbfd50b2dc163159ab120e68e
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-08-11 15:05:21 +0530

    Time: 0 ms (100%), Space: 8.2 MB (46.93%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -4,14 +4,10 @@ public:
 
         int count  = 0;
 
-        for(int i=0; i<32; i++){
-
-                if( (n & 1) ){
-                    count++;
-                }
-                n = n >> 1;
-
-
+        
+        while(n){
+            n = n & (n-1);
+            count++;
         }
 
         return count;
