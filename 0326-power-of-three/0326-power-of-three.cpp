8139cbb2fb04504b23dce0500f58ec2a612f6ed6
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-08-13 12:40:46 +0530

    Time: 0 ms (100%), Space: 8.8 MB (76.82%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -2,17 +2,10 @@ class Solution {
 public:
     bool isPowerOfThree(int n) {
 
+        int p = pow(3,19);
 
-        for(int i=0; i<=19; i++){
-            long p = pow(3,i);
 
-            if(n == p )
-            {
-                return true;
-            }
-        }
-
-        return false;
+            return n > 0 && (p%n == 0);
         
     }
 };
