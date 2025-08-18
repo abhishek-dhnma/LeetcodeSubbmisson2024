556a779a2a2cd100da508f640db3b53641eb48c9
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-08-19 01:02:25 +0530

    Time: 65 ms (18.5%), Space: 30.5 MB (19.6%) - LeetHub

--- 0679-24-game/0679-24-game.cpp
+++ 0679-24-game/0679-24-game.cpp
@@ -1,67 +1,60 @@
 class Solution {
 public:
-
     double epsilon = 0.0006;
 
-    bool solve(vector<double> & nums){
+    bool solve(vector<double>& nums) {
 
-        if(nums.size() == 1){
+        // BASE CASE
+
+        if (nums.size() == 1) {
+            // IMPORTANT
             return abs(nums[0] - 24.0) <= epsilon;
         }
 
+        // MAIN LOGIC
 
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < nums.size(); j++) {
 
-
-
-
-
-        for(int i = 0; i< nums.size(); i++ ){
-            for(int j = 0; j<nums.size(); j++){
-
-                if(i==j) continue;
+                if (i == j)
+                    continue;
                 vector<double> temp;
 
-
-
-                for(int k=0; k<nums.size(); k++){
-                    if(k != i && k != j ){
+                for (int k = 0; k < nums.size(); k++) {
+                    if (k != i && k != j) {
                         temp.push_back(nums[k]);
                     }
                 }
 
+                // operations options
+                //example : {k1, k2, ?}
+
                 double a = nums[i];
                 double b = nums[j];
 
-                // options 
-                vector<double> possVal = {a+b, a-b, b-a, a*b};
-
-                if(abs(b) > 0.0){
-                    possVal.push_back(a/b);
+                // options -  plus, substract, multiple, divison
+                vector<double> possVal = {a + b, a - b, b - a, a * b};
+                // dominator is not zero
+                if (abs(b) > 0.0) {
+                    possVal.push_back(a / b);
                 }
 
-                if(abs(a) > 0.0){
-                    possVal.push_back(b/a);
+                if (abs(a) > 0.0) {
+                    possVal.push_back(b / a);
                 }
 
-                
-                for(double val : possVal){
-                    temp.push_back(val); // do
-                    if(solve(temp) == true){ // explore
+                for (double val : possVal) {
+                    temp.push_back(val);       // do
+                    if (solve(temp) == true) { // explore
                         return true;
                     }
-                    temp.pop_back(); // undo 
-
+                    temp.pop_back(); // undo
                     // AKA BACKTRACKING
                 }
-
-
-
-
             }
         }
-
+        // No mathematical expression/combination found return false
         return false;
-
     }
 
     bool judgePoint24(vector<int>& cards) {
@@ -70,11 +63,10 @@ public:
 
         vector<double> nums;
 
-        for(int i=0; i<n; i++){
-            nums.push_back( 1.0 * cards[i]);
+        for (int i = 0; i < n; i++) {
+            nums.push_back(1.0 * cards[i]);
         }
 
         return solve(nums);
-        
     }
 };
