1c66535fd310f6b07e68fa85bacf333b5144d552
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-08-19 10:38:15 +0530

    Time: 0 ms (100%), Space: 111.4 MB (40.61%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
@@ -2,30 +2,22 @@ class Solution {
 public:
     long long zeroFilledSubarray(vector<int>& nums) {
         int n = nums.size();
-        int i = 0;;
-        int j=  0;
+        int i = 0;  // Start of current run of 0s
+        int j = 0;  // Current index to scan array
+        long long count = 0;  // Total number of subarrays
 
-        long long count =0;
-
-        while(j < n){
-
-            if(nums[j] == 0){
-                i = j;
-
-                while(j < n-1 && nums[j+1] == 0){
-                j++;
+        while (j < n) {
+            if (nums[j] == 0) {  // Start of a run of 0s
+                i = j;  // Mark start of run
+                while (j < n - 1 && nums[j + 1] == 0) {  // Extend run until non-0 or end
+                    j++;
+                }
+                int s = j - i + 1;  // Length of current run (m_i)
+                count += (s * (long long)(s + 1)) / 2;  // Add subarrays: m_i * (m_i + 1) / 2
             }
-
-            int s = j - i + 1;
-
-            count += (s * (long)(s+1))/2;
-            }
-            
-
-            j++;
+            j++;  // Move to next potential run
         }
 
         return count;
-        
     }
 };
