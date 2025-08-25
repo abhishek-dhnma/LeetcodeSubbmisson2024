158903b2350c20a2a03901a25516c987562ccdad
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-08-25 22:57:55 +0530

    Time: 15 ms (44.01%), Space: 31.2 MB (23.15%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.cpp
+++ 0498-diagonal-traverse/0498-diagonal-traverse.cpp
@@ -1,30 +1,25 @@
+
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
-
         int m = mat.size();
         int n = mat[0].size();
-
         vector<int> ans;
-
         map<int, vector<int>> mp;
 
+        // Group elements by diagonal index (i + j)
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-
                 int idx = i + j;
-
                 mp[idx].push_back(mat[i][j]);
             }
         }
 
+        // Process diagonals: reverse even-indexed diagonals, append to result
         for (auto& [k, v] : mp) {
-
             if (k % 2 == 0) {
-                // even
                 reverse(v.begin(), v.end());
             }
-
             for (auto& e : v) {
                 ans.push_back(e);
             }
