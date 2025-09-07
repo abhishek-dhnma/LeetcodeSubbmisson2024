8cd658852cd05c8c4b3a37faef7dc3f7bedcd92c
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-09-07 23:07:51 +0530

    Time: 0 ms (100%), Space: 9.7 MB (13.58%) - LeetHub

--- 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.cpp
+++ 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.cpp
@@ -2,36 +2,18 @@ class Solution {
 public:
     vector<int> sumZero(int n) {
 
-        vector<int> ans(n);
-
-        int mid = n / 2;
+        vector<int> ans;
         
-        bool isEven = true;
 
-        (n % 2 == 0) ? isEven = true : isEven = false;
+        for (int i = 1; i <=n; i+=2) {
 
-        for (int i = 0; i < n; i++) {
+            if (i + 1 <= n) {
 
-            if (i == mid) {
+                ans.push_back(i);
+                ans.push_back(-i);
 
-                if (isEven) {
-                    ans[i] = 1;
-                } else {
-                    ans[i] = 0;
-                }
-
-            } else if (i < mid) {
-
-                ans[i] = -(mid - i);
-
-            } else if (i > mid) {
-
-                if (isEven) {
-                    ans[i] = -(mid - i) + 1;
-
-                } else {
-                    ans[i] = -(mid - i);
-                }
+            } else {
+                ans.push_back(0);
             }
         }
 
