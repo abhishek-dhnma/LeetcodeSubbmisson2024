b6ceba54ca95be3f814beb060d4b8e09c8e838b6
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-09-07 22:51:28 +0530

    Time: 0 ms (100%), Space: 9.7 MB (13.58%) - LeetHub

--- 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.cpp
+++ 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.cpp
@@ -5,62 +5,40 @@ public:
         vector<int> ans(n);
 
         int l = 0;
-        int r = n-1;
+        int r = n - 1;
 
-        int mid = n/2;
+        int mid = n / 2;
         cout << mid;
 
-        if(n%2 == 0){
-            // even
+        bool isEven = true;
 
+        (n % 2 == 0) ? isEven = true : isEven = false;
 
+        for (int i = 0; i < n; i++) {
 
-            for(int i=0; i<n; i++){
+            if (i == mid) {
 
-                if(i ==  mid){
+                if (isEven) {
                     ans[i] = 1;
-                }else if(i < mid ){
-
-                    ans[i] = - (mid - i);
-
-
-                }else if( i > mid){
-
-                    ans[i] = - (mid - i) + 1;
-
-
-                }
-            }
-
-        }else{
-
-            //odd
-
-            
-
-            for(int i=0; i<n; i++){
-
-                if(i ==  mid){
+                } else {
                     ans[i] = 0;
-                }else if(i < mid){
-
-                  ans[i] = -(mid - i);
-
-
-                }else if ( i > mid){
-
-                    ans[i] = - (mid - i);
-
                 }
 
+            } else if (i < mid) {
 
+                ans[i] = -(mid - i);
+
+            } else if (i > mid) {
+
+                if (isEven) {
+                    ans[i] = -(mid - i) + 1;
+
+                } else {
+                    ans[i] = -(mid - i);
+                }
             }
-
-
         }
 
-
         return ans;
-        
     }
 };
