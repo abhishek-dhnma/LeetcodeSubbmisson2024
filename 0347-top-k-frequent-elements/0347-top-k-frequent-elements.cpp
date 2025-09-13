5858ce88dc800189985bcb947ee1befc8b5e5711
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-09-13 23:34:45 +0530

    Time: 0 ms (100%), Space: 18.1 MB (35.44%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -2,46 +2,41 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
 
-        // map - it will help me to count the freq
-        // heap - max heap - get the most freq elemt in const O(1)
-
+        // freq map
         unordered_map<int, int> freq;
-
-        //1 - 3;
 
         for(auto & n : nums){
 
             freq[n]++;
         }
 
-        // insert these int que 
+        // queue -> data structure
 
-        priority_queue< pair<int, int> > que; // max heap
+        priority_queue< pair<int, int> > que;
 
-        for(auto & [n, f] : freq ){
+        for(auto &  [n , f]  : freq){
 
-
-            que.push(make_pair(f, n));
+            que.push(  make_pair(f, n)   );
 
 
         }
+
+        // queue give me k most frequent elements
 
         vector<int> ans;
 
 
         while( k-- && !que.empty()){
 
-            pair<int, int> x = que.top();
+            // (f,n)
+        
+        ans.push_back(que.top().second);
 
-            ans.push_back(x.second);
-
-            que.pop();
+        que.pop();
 
         }
 
         return ans;
-
-
 
 
 
