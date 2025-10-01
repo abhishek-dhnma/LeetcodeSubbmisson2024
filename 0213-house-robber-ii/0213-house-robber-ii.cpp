534ccbfe02affdd7ba08af7912ced82547d6f67f
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-10-02 00:56:45 +0530

    Time: 0 ms (100%), Space: 10.1 MB (97.41%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
+    int t[101];
+    int solve(vector<int>& nums, int i, int end){
+
+    if(i > end){
+        return 0;
+    }
+        if(t[i] != -1) {
+            return t[i];
+        }
+
+        int take = nums[i] + solve(nums, i+2, end);
+        int skip = solve(nums, i+1, end);
+
+
+        return t[i] = max(take, skip);
+    }
+
     int rob(vector<int>& nums) {
+
         int n = nums.size();
-        
-        // Edge cases
-        if (n == 0) return 0;
-        if (n == 1) return nums[0];
-        
-        // Case 1: Rob houses from index 0 to n-2 (exclude last house)
-        vector<int> t1(n, 0);
-        t1[0] = nums[0];
-        if (n > 1) t1[1] = max(nums[0], nums[1]);
-        
-        for (int i = 2; i < n - 1; i++) {
-            int take = nums[i] + (i-2 >= 0 ? t1[i-2] : 0);
-            int skip = t1[i-1];
-            t1[i] = max(take, skip);
-        }
-        
-        int res1 = (n == 2) ? t1[1] : t1[n-2];
-        
-        // Case 2: Rob houses from index 1 to n-1 (exclude first house)
-        vector<int> t2(n, 0);
-        t2[1] = nums[1];
-        
-        for (int i = 2; i < n; i++) {
-            int take = nums[i] + (i-2 >= 1 ? t2[i-2] : 0);
-            int skip = t2[i-1];
-            t2[i] = max(take, skip);
-        }
-        
-        int res2 = t2[n-1];
-        
-        // Return the maximum of the two cases
-        return max(res1, res2);
+
+        if(n==1) return nums[0];
+
+        if(n==2) return max(nums[0], nums[1]);
+
+
+        memset(t, -1, sizeof(t));
+
+
+        int take_includefirsthouse = solve(nums, 0, n-2);
+
+memset(t, -1, sizeof(t));
+        int take_skipfirsthouse = solve(nums, 1, n-1);
+
+        return max(take_includefirsthouse, take_skipfirsthouse);
     }
 };
