9388090b93975f5532735a7db73f4f592a77c327
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-10-01 22:36:29 +0530

    Time: 11 ms (25.94%), Space: 13.4 MB (37.61%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,37 +1,43 @@
 class Solution {
 public:
 
-    
-
+    unordered_map<char, int> roman;
 
     int romanToInt(string s) {
 
+        roman['I'] = 1;
+        roman['V'] = 5;
+        roman['X'] = 10;
+        roman['L'] = 50;
+        roman['C'] = 100;
+        roman['D'] = 500;
+        roman['M'] = 1000;
+
+
+        // main logic
+
+        int i = 0;
+        int n = s.size();
+
         int ans = 0;
 
-        unordered_map<char, int> roman;
 
-    roman['I'] = 1;
-    roman['V'] = 5;
-    roman['X'] = 10;
-    roman['L'] = 50;
-    roman['C'] = 100;
-    roman['D'] = 500;
-    roman['M'] = 1000;
+        while( i <n){
 
-    int i = 0;
+            char ch1 = s[i];
+            char ch2 = s[i+1];
 
 
-        while( i < s.size()){
+            if( roman[ch1] < roman[ch2]){
 
-           char ch1 = s[i];
-           char ch2 = s[i+1];
-           if(roman[ch2] > roman[ch1]){
-            ans +=  (roman[ch2] - roman[ch1]);
+            ans += ( roman[ch2] - roman[ch1] );
             i+=2;
-           }else{
-            ans += roman[ch1];
-            i+=1;
-           }
+
+            }else {
+
+                ans +=  roman[ch1];
+                i+=1;
+            }
 
 
         }
@@ -40,3 +46,4 @@ public:
         
     }
 };
+
