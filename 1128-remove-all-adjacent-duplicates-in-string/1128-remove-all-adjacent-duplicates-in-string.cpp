1b467721ae97658a29bf406fdcf9d312b2977158
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-10-04 23:07:40 +0530

    Time: 5 ms (59.44%), Space: 13.7 MB (60.53%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
@@ -4,15 +4,22 @@ public:
 
         string ans = "";
 
+        for(auto & ch :  s){
 
-        for(auto & ch : s){
-
-            if(!ans.empty() && ans.back() == ch ){
+            if( !ans.empty()  && ans.back() == ch){
+                // equal
                 ans.pop_back();
-            }else{
+                
+            }else {
+
+                //not equal
 
                 ans.push_back(ch);
+
+
             }
+
+
         }
 
         return ans;
