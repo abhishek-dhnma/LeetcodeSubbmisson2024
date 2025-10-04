98072460587ce0e02a2c1e59fb46cbe905fdb73d
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-10-04 22:44:05 +0530

    Time: 0 ms (100%), Space: 9.3 MB (6.23%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,28 +1,64 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-
         int n = s.size();
+
         int m = t.size();
 
-        int i = 0;
-        int j = 0;
+        map<char, vector<int> > mp;
 
-        // iterate and check if match ot not
-        while (i < n && j < m) {
-            // match condition
-            if (s[i] == t[j]) {
-                i++;
+
+        // insert all t into map
+        for(int i=0; i<m; i++){
+
+            mp[ t[i] ].push_back(i);
+        }
+
+
+        // logic 
+
+
+        int previndex = -1;
+
+        // iterating s here every s char into map
+        for(int i = 0; i<n; i++){
+
+
+            char ch = s[i];
+
+            // not present
+
+            if(mp.find(ch) == mp.end()){
+
+                return false;
+
             }
 
-            j++;
+
+            // if present - in map
+
+            vector<int> idx = mp[ch];
+
+            // next larger value 
+
+            auto it = upper_bound(idx.begin(), idx.end(), previndex);
+
+            if( it ==  idx.end()) {
+// same logic but differn way 
+
+                return false;
+            }
+
+            previndex = *it;
+
+            // true
         }
 
-        if (i == n) {
+// s is subseq of t
+        return true;
 
-            return true;
-        }
 
-        return false;
+
+        
     }
 };
