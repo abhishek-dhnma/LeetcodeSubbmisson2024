a02a049410f4cbaa894bdebd6e3e88f2af74922a
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-10-06 17:36:19 +0530

    Time: 258 ms (43.89%), Space: 157.2 MB (38.26%) - LeetHub

--- 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
     long long maxAlternatingSum(vector<int>& nums) {
-
-        // Bottom Up --> past answers se apna -> future ka solution banate hai
-
-        
-
         int n = nums.size();
+        if (n == 0) return 0; // guard if empty input
 
-        vector<vector<long long>> dp(n, vector<long long>(2,0)); // even : 1 and odd : 0 and size n liya h
+        // dp[i][0] -> best alternating-sum considering nums[0..i] and the last operation is '-' (flag = 0)
+        // dp[i][1] -> best alternating-sum considering nums[0..i] and the last operation is '+' (flag = 1)
+        // We consider subsequences, not necessarily contiguous.
+        vector<vector<long long>> dp(n, vector<long long>(2, 0));
 
-        dp[0][0] = 0;  // flag:false (substract)  - you can't subtract first, so 0
-        dp[0][1] = nums[0];   // flag:true (add) 
+        // Base:
+        // Can't end with '-' at i=0 since there's no prior '+', so dp[0][0] = 0 (equivalent to not taking anything)
+        dp[0][0] = 0;
+        // Ending with '+' at i=0 means take nums[0]
+        dp[0][1] = nums[0];
 
-        for(int i=1; i<n; i++){
-            
-            // even case
-            dp[i][0] = max(dp[i-1][1] - nums[i], dp[i-1][0]);
+        for (int i = 1; i < n; ++i) {
+            // If we want the best that ends with '-' at i:
+            // either keep previous '-' (don't take nums[i]) OR take nums[i] as a '-' after a previous '+'
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1] - (long long)nums[i]);
 
-            // odd case
-            dp[i][1] = max(dp[i-1][0] + nums[i], dp[i-1][1]);
+            // If we want the best that ends with '+' at i:
+            // either keep previous '+' (don't take nums[i]) OR take nums[i] as a '+' after a previous '-'
+            dp[i][1] = max(dp[i-1][1], dp[i-1][0] + (long long)nums[i]);
         }
 
+        // Answer is best alternating-sum that ends with a '+'
         return dp[n-1][1];
-
-       // TC : O(N)
-       // SC : O(N)
-        
     }
 };
