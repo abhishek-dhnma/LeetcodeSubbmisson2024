e90f91c63348844a68728af868074f47d5a8dbca
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-10-06 23:41:41 +0530

    Time: 443 ms (5.08%), Space: 54 MB (21.96%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -1,27 +1,44 @@
 class Solution {
 public:
-    int findLongestChain(vector<vector<int>>& pairs) {
+    // Top-Down : LIS : longest Increasing Subsequence
 
-        int n = pairs.size();
+    int n;
+    int dp[2501][2501];
 
-        sort(pairs.begin(), pairs.end());
+    int findLIS(vector<vector<int>>& pairs, int curridx, int preidx) {
 
-        vector<int> dp(n,1);
-        int maxLIS = 1;
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<i; j++){
-
-                if(pairs[j][1] < pairs[i][0]){
- 
-                    dp[i] = max(dp[i], dp[j]+1);
-                    maxLIS = max(dp[i], maxLIS);
-
-                }
-            }
+        if (curridx >= n) {
+            return 0;
         }
 
-        return maxLIS;
-        
+        if (preidx != -1 && dp[preidx][curridx] != -1) {
+            return dp[preidx][curridx];
+        }
+
+        int take_include = 0;
+        if (preidx == -1 || pairs[preidx][1] < pairs[curridx][0]) {
+            take_include = 1 + findLIS(pairs, curridx + 1, curridx);
+        }
+
+        int skip_exclude = findLIS(pairs, curridx + 1, preidx);
+
+        if (preidx != -1) {
+            dp[preidx][curridx] = max(take_include, skip_exclude);
+        }
+
+        return max(take_include, skip_exclude);
+    }
+
+    int findLongestChain(vector<vector<int>>& pairs) {
+
+        // Top-Down Approach
+        n = pairs.size();
+        int curridx = 0;
+        int preidx = -1;
+        memset(dp, -1, sizeof(dp));
+
+        // sorting
+        sort(pairs.begin(), pairs.end());
+        return findLIS(pairs, curridx, preidx);
     }
 };
