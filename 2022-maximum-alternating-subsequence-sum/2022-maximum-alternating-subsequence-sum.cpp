fb8cd0765510930bc9d2b8745f521f7a10441325
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-10-06 17:14:32 +0530

    Time: 136 ms (73.29%), Space: 98.2 MB (81.44%) - LeetHub

--- 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.cpp
@@ -1,46 +1,23 @@
 class Solution {
 public:
-    
-
-    long long dp[1000001][2];
-
-
-    long long solve(vector<int>& nums, int i, bool flag){
-
-
-        // base case
-
-        if(i >= nums.size()){
-            return 0;
-        }
-        if(dp[i][flag] != -1){
-            return dp[i][flag];
-        }
-
-
-        // recursion
-
-        long long skip = solve(nums,i+1, flag);
-
-
-        long long take = solve(nums, i+1, !flag) + (flag ? nums[i] : -nums[i]);
-
-  
-        return dp[i][flag] = max(skip, take);
-
-
-    }
-
     long long maxAlternatingSum(vector<int>& nums) {
 
-        memset(dp, -1, sizeof(dp));
+        // Bottom Up --> past answers se apna -> future ka solution banate hai
 
-        return  solve(nums, 0, true);
+        long long dp[1000001][2];
 
-        // recursion : TC : O(2^N * N)
-        // with memo : TC : O(N)
+        int n = nums.size();
 
-        
+        for(int i=1; i<n+1; i++){
+            
+            // even case
+            dp[i][0] = max(dp[i-1][1] - nums[i-1], dp[i-1][0]);
+
+            // odd case
+            dp[i][1] = max(dp[i-1][0] + nums[i-1], dp[i-1][1]);
+        }
+
+        return max(dp[n][0],dp[n][1]);
         
     }
 };
