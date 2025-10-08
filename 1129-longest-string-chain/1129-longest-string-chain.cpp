cb8a84e469cfeed075fbcefdb541b7c647fb1e50
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-10-08 08:57:37 +0530

    Time: 1924 ms (8.13%), Space: 640.3 MB (21.88%) - LeetHub

---
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    bool isPredecessor(string s1, string s2) {
+
+        if (s2.size() != s1.size() + 1) return false;
+
+        int i = 0;
+        int j = 0;
+
+        int n = s1.size();
+        int m = s2.size();
+        int c = 0;
+
+        while (i < n && j < m) {
+
+            if (s1[i] == s2[j]) {
+                i++;
+                j++;
+            } else {
+                j++;
+                c++;
+                if(c > 1)return false;
+            }
+        }
+
+    
+        return true;
+    }
+
+    int longestStrChain(vector<string>& words) {
+
+        int n = words.size();
+
+        vector<int> countLIS(n, 1);
+
+        // sorting based on length
+        sort(words.begin(), words.end(), [](string& s1, string& s2) {
+            return s1.length() < s2.length();
+        });
+
+        // LIS
+
+        int maxLISResult = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+
+                if (isPredecessor(words[j], words[i])) {
+
+                    countLIS[i] = max(countLIS[i], countLIS[j] + 1);
+                    maxLISResult = max(maxLISResult, countLIS[i]);
+                }
+            }
+        }
+
+        return maxLISResult;
+    }
+};
