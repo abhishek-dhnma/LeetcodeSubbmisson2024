c326ac0a1a02d9445f06067563a3cf06fae3a01d
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-10-08 17:12:40 +0530

    Time: 0 ms (100%), Space: 14.1 MB (79.32%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -2,22 +2,24 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
 
+        vector<int> patienceSortedTechnique;
+
         int n = nums.size();
-        vector<int> dp(n+1,1);
 
-        int maxLIS = 1;
+        for(int i=0; i<n; i++){
+            
+            auto it = lower_bound(patienceSortedTechnique.begin(), patienceSortedTechnique.end(), nums[i]);
+            if(it != patienceSortedTechnique.end()){
 
-        for(int i=1; i<n; i++){
-            for(int j=0; j<i; j++){
-                if(nums[j] < nums[i]){
-                    dp[i] = max( dp[i]  , dp[j] + 1);
-                    maxLIS = max(maxLIS, dp[i]);
-                }
+                *it = nums[i];
 
+            }else{
+                patienceSortedTechnique.push_back(nums[i]);
             }
+
+
         }
 
-        return maxLIS;
-        
+        return patienceSortedTechnique.size();        
     }
-}; 
+};
