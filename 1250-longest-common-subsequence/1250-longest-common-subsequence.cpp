bff67e8dd4a6e5e9a6138f9129b3d8385cb00b9a
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-10-08 22:31:39 +0530

    Time: 27 ms (56.65%), Space: 27.4 MB (52.69%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -7,6 +7,7 @@ public:
         // bottom up approach 
 
         // 2d dp on string
+        
         int n = text1.size();
         int m = text2.size();
 
@@ -17,12 +18,36 @@ public:
             for(int j=1;j<=m; j++){
 
                 if(text1[i-1] == text2[j-1]){
+                    // if matching 
                     dp[i][j] = 1 + dp[i-1][j-1];
                 }else{
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
+
+        // printing LIS String
+
+        int i = n;
+        int j = m;
+        string printLIS = "";
+
+        while(i > 0 && j > 0){
+            if(text1[i-1] == text2[j-1]){
+                printLIS.push_back(text1[i-1] );
+                i--;
+                j--; // going diagonal
+            }else {
+                if(dp[i-1][j] > dp[i][j-1]){
+                    i--;
+                }else {
+                    j--;
+                }
+            }
+        }
+        reverse(printLIS.begin(), printLIS.end());
+
+        cout << printLIS;
 
         return dp[n][m];
         
