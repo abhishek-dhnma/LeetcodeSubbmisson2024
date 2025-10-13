f181aae4505f27edbae6cefcc68458414ba9b6dc
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-10-13 22:35:17 +0530

    Time: 0 ms (100%), Space: 17.1 MB (29.19%) - LeetHub

--- 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
@@ -4,33 +4,30 @@ public:
 
         int n = word.size();
 
-        vector<bool> vec(n, true);
-        vector<string> words = word;
-
-        sort(words[0].begin(), words[0].end());
-
-        for (int i = 1; i < n; i++) {
-
-            sort(words[i].begin(), words[i].end());
-
-            int pre = i - 1;
-            while (pre > 0 && vec[pre] == false) {
-                pre--;
-            }
-
-            if (words[pre] == words[i]) {
-                vec[i] = false;
-            }
-        }
-
         vector<string> result;
 
-        for(int i=0; i<n; i++){
-            if(vec[i] == true){
-                result.push_back(word[i]);
-            }
+        vector<string> ans;
+
+        vector<string> words = word;
+
+        
+
+        sort(words[0].begin(), words[0].end()); 
+    
+        result.push_back(words[0]);
+        ans.push_back(word[0]);
+        for (int i = 1; i < n; i++) { 
+
+            sort(words[i].begin(), words[i].end()); 
+
+
+
+            if (result.back() != words[i]) {
+                result.push_back(words[i]);
+                ans.push_back(word[i]);
+            } 
         }
 
-        return result;
+        return ans;
     }
 };
