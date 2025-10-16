5f8d475f572fe622218de2a635491fadcf969eb9
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-10-17 02:51:31 +0530

    Time: 8 ms (68.7%), Space: 121.3 MB (53.25%) - LeetHub

--- 2661-smallest-missing-non-negative-integer-after-operations/2661-smallest-missing-non-negative-integer-after-operations.cpp
+++ 2661-smallest-missing-non-negative-integer-after-operations/2661-smallest-missing-non-negative-integer-after-operations.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     int findSmallestInteger(vector<int>& nums, int value) {
-
-        unordered_map<int, int> freq;
-        int n = nums.size();
-
-        for(int i=0; i<n; i++){
-
-            int temp = ((nums[i]%value)+value)%value;
-
-            freq[temp]++;
+        // Count how many numbers fall into each remainder bucket [0..value-1]
+        vector<long long> cnt(value, 0);
+        for (long long x : nums) {
+            // normalize negative mods
+            int r = (int)((x % value + value) % value);
+            cnt[r]++;
         }
 
-
-        int mex = 0;
-
-        while(freq[mex%value] > 0 ){
-            freq[mex%value]--;
-            mex++;
+        // Greedily try to form 0,1,2,... in order.
+        // For i, we need a number from bucket (i % value).
+        // If the bucket has stock, consume one and move on.
+        for (long long i = 0;; ++i) {
+            int r = (int)(i % value);
+            if (cnt[r] > 0) {
+                cnt[r]--;
+            } else {
+                return (int)i; // first i we cannot form => MEX
+            }
         }
-
-        return mex;
-        
     }
 };
