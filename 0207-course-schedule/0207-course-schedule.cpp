6091137c607eefbdd75e931b5ab53b6c54fda454
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-10-23 14:42:33 +0530

    Time: 1 ms (86.34%), Space: 19.2 MB (85.76%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,62 +1,73 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& adj, int V, vector<bool>& visited,
-             vector<bool> & onStack) {
+
+
+    bool dfs( vector<vector<int>>& adj, int  V,vector<bool>& visited,  vector<bool>& onstack){
+
 
         visited[V] = true;
-        onStack[V] = true;
+        onstack[V] = true;
 
-        for (auto& v : adj[V]) {
 
-            if ( onStack[v]) {
+        for(auto & v : adj[V]){
+
+            if(onstack[v]){
                 return true;
-                
             }
 
 
             if(!visited[v]){
-
-                if(dfs(adj, v, visited, onStack)){
+                if(dfs(adj, v, visited, onstack)){
                     return true;
                 }
-
             }
+
         }
 
-         onStack[V] = false;
+
+
+        onstack[V]= false;
         return false;
 
-        
+
+
     }
 
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites ) {
+int n = numCourses;
+        vector<vector<int>> adj(n);
+
+        
 
 
-        vector<vector<int>> adj(numCourses);
+        for(auto & cour : prerequisites){
 
-        for (auto& link : prerequisites) {
-
-            int u = link[1];
-            int v = link[0];
+            int u = cour[1];
+            int v = cour[0];
 
             adj[u].push_back(v);
+
         }
 
-        vector<bool> visited(numCourses, false);
-        vector<bool> onStack(numCourses, false);
+        vector<bool> visited(n, false);
+        vector<bool> onstack(n, false);
 
-        for (int i = 0; i < numCourses; i++) {
 
-            if (!visited[i]) {
+        for(int i=0; i<n; i++){
 
-                if(dfs(adj, i, visited, onStack)){
+            if(!visited[i]){
+
+                if(dfs(adj, i, visited, onstack)){
                     return false;
                 }
+
             }
+
+
         }
 
-       
-
         return true;
+        
     }
 };
