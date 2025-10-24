c78a64a49b9e35f8a1fadaf68457a765bb680777
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-10-24 23:34:03 +0530

    Time: 0 ms (100%), Space: 18 MB (20.79%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -2,72 +2,88 @@ class Solution {
 public:
     int n;
 
-    int FindRotatedIndex(vector<int>& nums) {
+    int findpivot(vector<int>& nums) {
 
         int l = 0;
         int r = n - 1;
 
         while (l < r) {
 
+            // remove duplicated left
+
             while(l < r && nums[l] == nums[l+1]){
                 l++;
             }
 
-            while(l <r && nums[r] == nums[r-1]){
+
+            // remove duplicate right side 
+
+            while(l < r && nums[r] == nums[r-1]){
                 r--;
             }
 
-
+            
             int mid = l + (r - l) / 2;
 
             if (nums[mid] > nums[r]) {
-                l = mid + 1;
-            } else {
-                r = mid;
+                // non sorted part
+
+                l = mid + 1; //  right
+            }else
+            {
+                r = mid; // left
             }
         }
+
         return r;
     }
 
-    bool binarySearch(vector<int>& nums, int l, int r, int target) {
+    int binarysearch(vector<int>& nums, int l, int r, int target) {
 
         while (l <= r) {
 
-            
-
             int mid = l + (r - l) / 2;
 
-            if (nums[mid] == target) {
-                return true;
-            } else if (nums[mid] > target) {
+            // move
+
+            if (nums[mid] > target) {
+                // left
 
                 r = mid - 1;
-
-            } else {
+            } else if (nums[mid] < target) {
+                // right
                 l = mid + 1;
+            } else {
+                // both same
+                return mid;
             }
         }
 
-        return false;
+        return -1;
     }
 
-    bool search(vector<int>& nums, int target) {
+    int search(vector<int>& nums, int target) {
+
+        // binary seaarch
 
         n = nums.size();
 
-        // find pivot
-        int pivot = FindRotatedIndex(nums);
+        int pivot = findpivot(nums);
 
-        // search in left and right side
+        cout << pivot << endl;
 
-        bool isPresent = binarySearch(nums, 0, pivot-1, target);
+        int idx = -1;
 
-        if (isPresent) {
+        idx = binarysearch(nums, 0, pivot - 1, target);
+
+        if (idx != -1)
             return true;
-        }
 
-        isPresent = binarySearch(nums, pivot , n - 1, target);
+        idx = binarysearch(nums, pivot, n - 1, target);
 
-        return isPresent;
+         if (idx != -1)
+            return true;
+
+        return false;
     }
 };
