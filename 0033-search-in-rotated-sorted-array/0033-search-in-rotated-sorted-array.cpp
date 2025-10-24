0510f997899642543de41fe3ba63210f9c02ec22
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-10-24 23:06:53 +0530

    Time: 0 ms (100%), Space: 15.3 MB (32.71%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -2,61 +2,70 @@ class Solution {
 public:
     int n;
 
-    int findpivot(vector<int>& nums){
+    int findpivot(vector<int>& nums) {
 
         int l = 0;
-        int r = n-1;
+        int r = n - 1;
 
-        while(l <r){
-            int mid = l + (r-l)/2;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
 
-            if(nums[mid] > nums[r]){
-                l = mid +1;
-            }else{
-                r = mid;
+            if (nums[mid] > nums[r]) {
+                // non sorted part
+
+                l = mid + 1; //  right
+            }else
+            {
+                r = mid; // left
             }
         }
 
-        return r;
-
+        return l;
     }
 
-    int binarySearch(vector<int>& nums, int l, int r, int target){
+    int binarysearch(vector<int>& nums, int l, int r, int target) {
 
-        while(l <= r){
-            int mid = l + (r-l)/2;
+        while (l <= r) {
 
-            if(nums[mid] == target){
+            int mid = l + (r - l) / 2;
+
+            // move
+
+            if (nums[mid] > target) {
+                // left
+
+                r = mid - 1;
+            } else if (nums[mid] < target) {
+                // right
+                l = mid + 1;
+            } else {
+                // both same
                 return mid;
-            }else if(nums[mid] < target){
-
-                l = mid+1;
-
-            }else{
-                r = mid-1;
             }
         }
 
-
-        return -1;   
-
+        return -1;
     }
-
 
     int search(vector<int>& nums, int target) {
 
+        // binary seaarch
+
         n = nums.size();
 
-        int pivot = findpivot(nums); // similar to find minimum in rotated sorted array
+        int pivot = findpivot(nums);
 
+        cout << pivot << endl;
 
-        // find target in left side 
-        int idx = binarySearch(nums,0, pivot-1, target);
+        int idx = -1;
 
-        if(idx != -1) return idx;
-        // find target in right side
-        idx = binarySearch(nums,pivot, n-1, target);
+        idx = binarysearch(nums, 0, pivot - 1, target);
 
-        return idx;        
+        if (idx != -1)
+            return idx;
+
+        idx = binarysearch(nums, pivot, n - 1, target);
+
+        return idx;
     }
 };
