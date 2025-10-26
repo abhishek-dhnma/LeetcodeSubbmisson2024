f7bd0008b1c744a944bae38941177149059b7ca0
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-10-26 23:24:32 +0530

    Time: 0 ms (100%), Space: 17.1 MB (45.84%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,67 +1,58 @@
 class Solution {
 public:
-    vector<vector<int>> dir = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+    vector<vector<int>> directions = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
 
     int orangesRotting(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
 
-        int n = grid.size();
-        int m = grid[0].size();
+        queue<pair<int, int>> rottenQueue;
+        int freshCount = 0;
 
-        queue<pair<int, int>> q;
-        int freshOrange = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if(grid[i][j] == 1){
-                    freshOrange++;
-                }
-                else if (grid[i][j] == 2) {
-                    q.push({i, j});
+        // Step 1: Count fresh oranges and push all rotten ones
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == 1) {
+                    freshCount++;
+                } else if (grid[r][c] == 2) {
+                    rottenQueue.push({r, c});
                 }
             }
         }
 
-        if(freshOrange == 0){
-            return 0;
-        }
+        if (freshCount == 0) return 0;
 
-        int mins = 0;
+        int minutesPassed = 0;
 
-        // multi-source bfs
+        // Step 2: Multi-source BFS
+        while (!rottenQueue.empty()) {
+            int levelSize = rottenQueue.size();
 
-        while (!q.empty()) {
+            while (levelSize--) {
+                auto [row, col] = rottenQueue.front();
+                rottenQueue.pop();
 
-            int N = q.size();
+                for (auto& dir : directions) {
+                    int newRow = row + dir[0];
+                    int newCol = col + dir[1];
 
-            while (N--) {
+                    if (newRow >= 0 && newRow < rows &&
+                        newCol >= 0 && newCol < cols &&
+                        grid[newRow][newCol] == 1) {
 
-                auto cell = q.front();
-                q.pop();
-
-                int i = cell.first;
-                int j = cell.second;
-
-                for (auto d : dir) {
-                    int i_ = i + d[0];
-                    int j_ = j + d[1];
-
-                    if (i_ >= 0 && i_ < n && j_ >= 0 && j_ < m &&
-                        grid[i_][j_] == 1) {
-
-                            q.push({i_,j_});
-                            grid[i_][j_] = 2;
-                            freshOrange--;
-
+                        grid[newRow][newCol] = 2; // rot it
+                        freshCount--;
+                        rottenQueue.push({newRow, newCol});
                     }
                 }
             }
 
-            mins++;
+            minutesPassed++;
         }
 
-
-        if (freshOrange == 0) {
-            return mins==0 ? mins : mins-1;
+        // Step 3: Return result
+        if (freshCount == 0) {
+            return minutesPassed == 0 ? 0 : minutesPassed - 1;
         }
 
         return -1;
