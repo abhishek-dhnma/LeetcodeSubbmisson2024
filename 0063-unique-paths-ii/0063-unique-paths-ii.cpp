5273d2f88bd72a004292cec79bb6fbd285930220
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-11-19 19:27:52 +0530

    Time: 0 ms (100%), Space: 12 MB (26.76%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -5,48 +5,59 @@ public:
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
 
-        vector<vector<int>> gridDP(n+1 , vector<int>(m+1, 0));
+        // DP array of same size as grid
+        vector<vector<int>> dp(n, vector<int>(m, 0));
 
+        // If starting cell is blocked → no paths possible
         if(obstacleGrid[0][0] == 1) return 0;
 
-        gridDP[0][0] = 1;
+        dp[0][0] = 1;   // Starting position has exactly 1 way to be reached
 
+        // -------------------------
+        // Fill first row
+        // -------------------------
+        for(int j = 1; j < m; j++) {
 
-        // fill coloum
+            // If cell has obstacle → 0 ways
+            if(obstacleGrid[0][j] == 1)
+                dp[0][j] = 0;
 
-        for(int c=1; c<m; c++){
-            if(obstacleGrid[0][c] == 1){
-                gridDP[0][c] = 0;
-            }else{
-                gridDP[0][c] = gridDP[0][c-1];
-            }
-
+            // Otherwise, it can only come from left
+            else
+                dp[0][j] = dp[0][j-1];
         }
 
-        // fill rows
+        // -------------------------
+        // Fill first column
+        // -------------------------
+        for(int i = 1; i < n; i++) {
 
-        for(int r=1; r<n; r++){
-            if(obstacleGrid[r][0] == 1){
-                gridDP[r][0] = 0;
-            }else{
-                gridDP[r][0] = gridDP[r-1][0];
+            // If obstacle → 0 ways
+            if(obstacleGrid[i][0] == 1)
+                dp[i][0] = 0;
+
+            // Otherwise, it can only come from top
+            else
+                dp[i][0] = dp[i-1][0];
+        }
+
+        // -------------------------
+        // Fill the rest of the grid
+        // -------------------------
+        for(int i = 1; i < n; i++) {
+            for(int j = 1; j < m; j++) {
+
+                // If obstacle → no paths
+                if(obstacleGrid[i][j] == 1)
+                    dp[i][j] = 0;
+
+                // Otherwise sum of top + left
+                else
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
 
-        // fill gridDP matrix
-
-        for(int i=1; i<n; i++){
-            for(int j=1; j<m; j++){
-
-                if(obstacleGrid[i][j] == 1){
-                    gridDP[i][j] = 0;
-                }else{
-                    gridDP[i][j] = gridDP[i-1][j] + gridDP[i][j-1];
-                }
-            }
-        }
-
-        return gridDP[n-1][m-1];
-        
+        // Bottom-right cell gives final answer
+        return dp[n-1][m-1];
     }
 };
