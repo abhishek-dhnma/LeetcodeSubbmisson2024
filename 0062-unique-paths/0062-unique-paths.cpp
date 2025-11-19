1fa1e465d7d7ec652012e598562d4ebe2caa2de4
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-11-19 16:11:46 +0530

    Time: 0 ms (100%), Space: 9.3 MB (63.92%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,39 +1,21 @@
 class Solution {
 public:
+    int uniquePaths(int m, int n) {
 
-    vector<vector<int>> dir = {{1,0}, {0,1}}; // D R
-    int dp[101][101];
+        // Bottom Up Approach
 
-    int dfs(int m, int n, int i, int j){
+        vector<vector<int>> dp(m, vector<int>(n, 1));
 
+        dp[0][0] = 1;
+        // # of ways to reach at that cell from down and right
 
-        // base case destination
-        if(i==m-1 && j == n-1){
-            return 1;
-        }
-
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-
-        int ways = 0;
-        for(auto& d : dir){
-            int i_ = i + d[0];
-            int j_ = j + d[1];
-
-            if(i_ >= 0 && i_ <m && j_ >=0 && j_ <n){
-               ways += dfs(m, n, i_, j_);
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
 
-        return dp[i][j] = ways;
-
-    }
-
-    int uniquePaths(int m, int n) {
-
-        memset(dp, -1, sizeof(dp));
-        return dfs(m, n , 0, 0);
+        return dp[m-1][n-1];
         
     }
 };
