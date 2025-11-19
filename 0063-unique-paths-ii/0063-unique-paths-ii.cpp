06fc66ddc85a67ac01c6714c7a68e5fdb4a61e54
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-11-19 19:13:16 +0530

    Time: 0 ms (100%), Space: 11.9 MB (26.76%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,55 +1,60 @@
 class Solution {
 public:
-
-    vector<vector<int>> dir = {{1,0}, {0,1}}; // D R 
-
-    int dp[101][101];
-
-    int dfs(vector<vector<int>>& obstacleGrid, int n, int m, int i, int j){
-
-
-        // base case and last cell also obstacle cell
-        if(i == n-1 && j == m-1 && obstacleGrid[i][j] != 1){
-            return 1;
-        }
-
-        // if any cell is obstacle cell return from there
-        if(obstacleGrid[i][j] ==1){
-            return 0;
-        }
-
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-
-
-        int ways = 0;
-        for(auto & d : dir){
-
-            int i_ = i + d[0]; 
-            int j_ = j + d[1];
-
-
-            if(i_ >= 0 && i_ <n && j_ >= 0 && j_ < m ){
-                ways += dfs(obstacleGrid, n, m, i_, j_);
-            }
-
-
-        }
-
-        return dp[i][j] = ways;
-
-    }
-
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-
 
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
 
-        memset(dp, -1, sizeof(dp));
-        
-        return dfs(obstacleGrid, n, m, 0, 0);
+        vector<vector<int>> gridDP(n+1, vector<int>(m+1, -1));
+
+        // fill first Coloumn
+
+        for(int c=0; c<m; c++){
+            
+            if( c >= 1 && obstacleGrid[0][c-1] == 1){
+                gridDP[0][c] = 0;
+                obstacleGrid[0][c] = 1;
+            }else if(obstacleGrid[0][c] == 1 ){
+                    gridDP[0][c] = 0;
+            }else{
+                    gridDP[0][c] = 1;
+            }
+
+        } 
+
+
+        // fill first Row
+
+         for(int r=0; r<n; r++){
+            
+            if( r >= 1 && obstacleGrid[r-1][0] == 1){
+                gridDP[r][0] = 0;
+                obstacleGrid[r][0] = 1;
+            }else if(obstacleGrid[r][0] == 1){
+                    gridDP[r][0] = 0;
+            }else{
+                    gridDP[r][0] = 1;
+            }
+
+        } 
+
+
+        // fill gridDP
+
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+
+                if(obstacleGrid[i][j] == 1 ){
+                    gridDP[i][j] = 0;
+                }else {
+                    gridDP[i][j] = gridDP[i][j-1] + gridDP[i-1][j];
+                }
+
+            }
+        }
+
+
+        return gridDP[n-1][m-1];
 
         
     }
