07fb3a9881bd2bb16a879a512607ab43d4ae6136
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-11-24 02:58:35 +0530

    Time: 40 ms (33.66%), Space: 46.1 MB (7.28%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
@@ -1,36 +1,29 @@
 class Solution {
 public:
-
     vector<vector<int>> dp;
 
-
     int solve(int i, int rem, vector<int>& nums){
-
         if(i >= nums.size()){
-            if(rem == 0){
-                return 0;
-            }
-
-            return INT_MIN;
+            return (rem == 0) ? 0 : INT_MIN;
         }
 
         if(dp[i][rem] != -1){
             return dp[i][rem];
         }
 
-        int take = nums[i] + solve(i+1, (rem + nums[i])%3, nums);
+        // safe: ask child first
+        int childTake = solve(i+1, (rem + nums[i]) % 3, nums);
+        int take = (childTake == INT_MIN) ? INT_MIN : nums[i] + childTake;
+
         int skip = solve(i+1, rem, nums);
 
-
-        return  dp[i][rem] = max(take, skip);
+        return dp[i][rem] = max(take, skip);
     }
-    
+
     int maxSumDivThree(vector<int>& nums) {
-
         int n = nums.size();
-
-        dp.assign(n, vector<int>(3,-1));
-
-        return solve(0, 0, nums);
+        dp.assign(n, vector<int>(3, -1));
+        int ans = solve(0, 0, nums);
+        return max(0, ans); // ensure non-negative
     }
 };
