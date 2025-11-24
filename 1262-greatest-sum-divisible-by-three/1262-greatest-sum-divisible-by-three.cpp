83920cb1ecb9ac2dbbfebbf943ab5f87521f27b3
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-11-24 15:49:21 +0530

    Time: 3 ms (86.73%), Space: 28.6 MB (98.54%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
@@ -4,56 +4,36 @@ public:
 
         // Bottom-Up Approach space optimization
 
-        int n =  nums.size();
-
-        //vector<vector<int>> dp(2, vector<int>(3,-1));
+        int n = nums.size();
 
         vector<int> dp = {0, INT_MIN, INT_MIN};
 
         vector<int> new_dp(3);
 
+        // Filling
 
-        // // BASE CASES
-
-        // dp[1][0] = 0;
-
-        // dp[1][1] = INT_MIN; 
-
-        // dp[1][2] = INT_MIN;
-
-
-        // Filling the Matrix
-
-
-        for(int i=n-1; i>=0; i--){
+        for (int i = n - 1; i >= 0; i--) {
 
             fill(new_dp.begin(), new_dp.end(), INT_MIN);
- 
 
-            for(int rem=0; rem<3; rem++){
+            for (int rem = 0; rem < 3; rem++) {
 
                 // take
 
-                int newRem = (nums[i] + rem)%3;
+                int newRem = (nums[i] + rem) % 3;
 
-                int take =  nums[i] +  dp[newRem];
-                
-
+                int take = nums[i] + dp[newRem];
 
                 // skip
-            
+
                 int skip = dp[rem];
 
-                new_dp[rem] = max(take,skip);
-
+                new_dp[rem] = max(take, skip);
             }
 
             dp = new_dp;
         }
 
         return dp[0];
-
-        
-        
     }
 };
