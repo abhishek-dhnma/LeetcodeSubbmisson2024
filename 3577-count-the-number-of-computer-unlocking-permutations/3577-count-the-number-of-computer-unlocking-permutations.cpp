598ed569bb23c5bf9ba136cc64aa31499321daca
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-12-10 20:46:47 +0530

    Time: 0 ms (100%), Space: 92.8 MB (71%) - LeetHub

--- 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.cpp
+++ 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.cpp
@@ -1,38 +1,21 @@
 class Solution {
 public:
-long long M;
-
-long long factorial_mod(int n) {
-    if (n < 0) {
-        // Factorial is not defined for negative numbers
-        return 0; // Or throw an exception
-    }
-    if (n == 0 || n == 1) {
-        return 1;
-    }
-    // Calculate (n * factorial(n-1)) % M
-    return (n * factorial_mod(n - 1)) % M;
-}
-
     int countPermutations(vector<int>& complexity) {
 
+        int M = 1000000007;
+
+        long long ans = 1;
         int n = complexity.size();
 
-        M = 1000000007;
+        for(int i=1; i<n; i++){
 
-        auto min_el = *min_element(complexity.begin()+1, complexity.end());
+            if(complexity[0] >= complexity[i]) return 0;
 
-
-        if(min_el <= complexity[0]){
-            return 0;
+            ans *= i;
+            ans %= M;
         }
 
-        // Calculate factorial(n_value - 1) % modulus
-    long long result = factorial_mod(n - 1);
-
-        min_el = result;
-
-        return min_el;
+        return ans;
         
     }
 };
