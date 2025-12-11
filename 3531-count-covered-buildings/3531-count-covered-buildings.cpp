ba3fe8067624ddbc29b97b94fde8d5558fabb8c4
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-12-11 18:00:57 +0530

    Time: 15 ms (97.79%), Space: 283.9 MB (85.64%) - LeetHub

--- 3531-count-covered-buildings/3531-count-covered-buildings.cpp
+++ 3531-count-covered-buildings/3531-count-covered-buildings.cpp
@@ -1,51 +1,38 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     int countCoveredBuildings(int n, vector<vector<int>>& buildings) {
-        // edge-case: if no buildings
-        if (buildings.empty()) return 0;
 
-        // maps for O(n) min/max accumulation
-        unordered_map<int, pair<int,int>> x_to_yminmax; // x -> (minY, maxY)
-        unordered_map<int, pair<int,int>> y_to_xminmax; // y -> (minX, maxX)
+        vector<int> maxRow(n+1);
+        vector<int> minRow(n+1, n+1);
+        vector<int> maxCol(n+1);
+        vector<int> minCol(n+1, n+1);
 
-        // initialize maps
-        for (auto &b : buildings) {
-            int x = b[0], y = b[1];
+        for(auto & b : buildings){
 
-            // x -> minY, maxY
-            if (!x_to_yminmax.count(x)) x_to_yminmax[x] = {y, y};
-            else {
-                auto &pr = x_to_yminmax[x];
-                pr.first = min(pr.first, y);
-                pr.second = max(pr.second, y);
-            }
+            int x = b[0];
+            int y = b[1];
 
-            // y -> minX, maxX
-            if (!y_to_xminmax.count(y)) y_to_xminmax[y] = {x, x};
-            else {
-                auto &pr = y_to_xminmax[y];
-                pr.first = min(pr.first, x);
-                pr.second = max(pr.second, x);
-            }
+            maxRow[y] = max(maxRow[y], x);
+            minRow[y] = min(minRow[y], x);
+
+            maxCol[x] = max(maxCol[x], y);
+            minCol[x] = min(minCol[x], y);
+
         }
 
 
-        // count covered buildings
         int count = 0;
-        for (auto &b : buildings) {
-            int x = b[0], y = b[1];
-            // guaranteed to exist since we populated from buildings, but checking is cheap
-            const auto &yrange = x_to_yminmax.at(x); // (minY, maxY)
-            const auto &xrange = y_to_xminmax.at(y); // (minX, maxX)
-            // check: building strictly inside both ranges (not on boundary)
-            if (yrange.first < y && yrange.second > y && xrange.first < x && xrange.second > x) {
+        for(auto & b : buildings){
+            int x = b[0];
+            int y = b[1];
+
+            if(minRow[y] < x && maxRow[y] > x && minCol[x] < y && maxCol[x] > y ){
                 count++;
             }
+
         }
 
         return count;
+        
     }
 };
