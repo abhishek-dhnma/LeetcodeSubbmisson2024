e00ef40bb78411688685887c609b16662ec58b1f
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-12-23 02:37:56 +0530

    Time: 0 ms (100%), Space: 27.3 MB (51.44%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -3,7 +3,16 @@ public:
     void reverseString(vector<char>& s) {
 
 
-        reverse(s.begin(), s.end());
+        int i=0;
+        int j = s.size()-1;
+
+
+        while(i < j){
+
+            swap(s[i], s[j]);
+            i++;
+            j--;
+        }
         
     }
 };
