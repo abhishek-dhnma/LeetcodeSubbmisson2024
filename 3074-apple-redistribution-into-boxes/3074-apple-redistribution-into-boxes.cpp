8a2dcd647894ab6b0c831ba1581be58db6aa2e58
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2025-12-25 00:14:15 +0530

    Time: 0 ms (100%), Space: 33.1 MB (45.84%) - LeetHub

---
+++ 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
+
+        int ans = 0;
+
+        int n = apple.size();
+        int m = capacity.size();
+
+        sort(capacity.begin(), capacity.end(), greater<int>());
+
+        int totalapple = 0;
+
+        for (int i = 0; i < n; i++) {
+            totalapple += apple[i];
+        }
+
+
+        cout << totalapple;
+
+        for(int i=0; i<m; i++){
+
+            if(totalapple > 0){
+                int check = totalapple - capacity[i];
+
+                if(check > 0){
+                    ans++;
+                }else{
+                    break;
+                }
+
+                
+            totalapple = check;   
+            }
+
+        }
+
+        return ans+1;
+    }
+};
