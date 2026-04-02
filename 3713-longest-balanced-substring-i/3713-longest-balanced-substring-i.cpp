b0731fc8119e8b8c3e0ea19827475c67e06663f0
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2026-04-02 23:46:25 +0530

    Time: 92 ms (63.85%), Space: 25.5 MB (40%) - LeetHub

--- 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.cpp
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.cpp
@@ -5,40 +5,31 @@ public:
         int n = s.size();
 
         int maxans = 0;
-        
-        for(int i=0; i<n; i++){
-            vector<int> count(26,0);
-            for(int j=i; j<n; j++){
+
+        for (int i = 0; i < n; i++) {
+            vector<int> count(26, 0);
+            for (int j = i; j < n; j++) {
 
                 char curchar = s[j];
 
                 count[curchar - 'a']++;
 
                 int valid = true;
-                
-                for(int k=0; k<26; k++){
-                    
-                    if(count[k] > 0 && count[k] != count[curchar - 'a']){
-                        
-                    valid = false;
-                    break;
+
+                for (int k = 0; k < 26; k++) {
+
+                    if (count[k] != 0 && count[k] != count[curchar - 'a']) {
+                        valid = false;
+                        break;
                     }
                 }
 
-                if(valid){
-                    
-                        maxans = max(maxans, j-i+1);
-                    
+                if (valid) {
+                    maxans = max(maxans, j - i + 1);
                 }
-
-
-
             }
-
-
         }
 
         return maxans;
-        
     }
 };
