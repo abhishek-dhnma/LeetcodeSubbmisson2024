051432d8bb9a8d03197bd01cf3230aeff30bcf75
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2026-04-05 02:24:56 +0530

    Time: 43 ms (84.91%), Space: 29.1 MB (72.12%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,48 +1,62 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-
-        sort(nums.begin(), nums.end());
         int n = nums.size();
-
+        
+        // WHY SORT? 
+        // 1. It allows us to use the Two-Pointer technique (only works on sorted data).
+        // 2. It makes it easy to skip duplicate numbers because they will be adjacent.
+        // 3. It allows for an early exit optimization if the smallest number is > 0.
+        sort(nums.begin(), nums.end());
+        
         vector<vector<int>> ans;
 
+        for (int i = 0; i < n - 2; i++) {
+            
+            // WHY BREAK? 
+            // If the smallest of the three numbers is positive, their sum 
+            // will always be > 0. No need to check further.
+            if (nums[i] > 0) break;
 
-        for(int i=0; i<n; i++){
+            // WHY SKIP nums[i] DUPLICATES? 
+            // If we already tried this number as our 'fixed' first element,
+            // using it again would result in the exact same triplets.
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            if(i > 0 && nums[i] == nums[i-1]) continue;
+            int x = i + 1; // Left pointer
+            int y = n - 1; // Right pointer
 
-            int t = nums[i];
-            int x = i+1;
-            int y = n-1;
-
-            while(x < y){
-
+            while (x < y) {
                 int sum = nums[i] + nums[x] + nums[y];
-                if(0 ==  sum){
+
+                if (sum == 0) {
                     ans.push_back({nums[i], nums[x], nums[y]});
-                    while( x < y && nums[x] == nums[x+1])
-                    {x++;}
-                    while( x < y && nums[y] == nums[y-1]){
-                        y--;
-                    }
+                    
+                    // WHY SKIP nums[x] & nums[y] DUPLICATES?
+                    // We found a valid triplet. If the next x or previous y are the 
+                    // same numbers, they would produce the same triplet again.
+                    while (x < y && nums[x] == nums[x + 1]) x++;
+                    while (x < y && nums[y] == nums[y - 1]) y--;
 
+                    // After skipping duplicates, we still need to move to the next 
+                    // potential unique pair.
                     x++;
                     y--;
-                }else if(0 < sum){
-
-                    y--;
-
-                }else {
-
+                } 
+                else if (sum < 0) {
+                    // WHY x++? 
+                    // The sum is too small. Since the array is sorted, 
+                    // moving the left pointer to the right gives us a larger number.
                     x++;
+                } 
+                else {
+                    // WHY y--? 
+                    // The sum is too large. Moving the right pointer to the 
+                    // left gives us a smaller number.
+                    y--;
                 }
             }
-
         }
-
-
         return ans;
-        
     }
 };
