27782c9032e26946fc0b67ea998a805868578b1f
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2026-06-02 03:53:30 +0530

    Time: 5 ms (96.04%), Space: 44.4 MB (83.54%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.java
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.java
@@ -3,25 +3,18 @@ class Solution {
 
         int n = cost.length;
 
-        if(n == 0) return 0;
-
-        if(n == 1) return cost[0];
-
-        if(n == 2) return cost[0] + cost[1];
+        
 
         Arrays.sort(cost);
 
         int minCost = 0 ;
 
-        for(int i=n-1; i>=0; i-=2){
+        for(int i=n-1; i>=0; i--){
 
-            if(i == 0) {
+            if((n-1-i)%3 != 2) {
                 minCost += cost[i];
-            }else{
-            
-           minCost += (cost[i] + cost[i-1]);
-           i--; 
             }
+        
 
         }
 
