8b9ca7bc9a151da83b65d85957c59e34a785f17c
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2026-09-05 21:44:02 +0530

    Time: 4 ms (88.55%), Space: 133.4 MB (14.89%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -3,19 +3,23 @@ class Solution {
 
         int n  = nums.length;
 
-        int[] mx = new int[n];
         int[] mn = new int[n];
 
-        mx[0] = nums[0];
         mn[n-1] = nums[n-1];
 
-        for(int i=1,j=n-2; i<n && j>=0; i++, j--){
-            mx[i] = Math.max(mx[i-1], nums[i]);
+        for(int j=n-2;  j>=0; j--){
+
             mn[j] = Math.min(mn[j+1], nums[j]);
         }
 
+        int mx = nums[0];
+
         for(int i=0; i<n; i++){
-            if( k >= (mx[i] - mn[i])){
+
+            mx = Math.max(nums[i], mx);
+
+
+            if( k >= (mx - mn[i])){
                 return i;
             }
         }
